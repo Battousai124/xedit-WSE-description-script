1ff30f6ca9f1ff4becc6b8c5d585b85263064268
Gernash <gernash01@gmail.com>
2020-08-05 11:39:25 +1000

    Update Gernashs description Renamer.pas

--- Gernashs description Renamer.pas
+++ Gernashs description Renamer.pas
@@ -4,30 +4,37 @@
 // Author: Gernash
 //
 
-unit Renamer; //FO4PatchOmodDescriptions;
- 
+unit Gernashs description Renamer;
+
+  // code for editing xEdit scripts with Delphi
+interface
+
+implementation
+
+uses xEditAPI, Classes, SysUtils, StrUtils, Windows;
+
 const
   sPropertiesList = wbScriptsPath + 'Gernashs description Renamer - Resource.txt';
- 
+
 var
   slPropertyMap: TStringList;
   plugin: IInterface;
- 
+
 function GetMappedValue(prop: IInterface): String;
 var
   valuetype, valuefunctiontype, valuePropertytype: string;
   f: Real;
   g: String;
-  
+
 
 // OMOD Property Value Sort to %, x, deg or Value {{{THE MATHS}}}
 
-  
+
 begin
 	valuetype := GetElementEditValues(prop, 'Value Type');
   valuePropertytype := GetElementEditValues(prop, 'Property');
   valuefunctiontype := GetElementEditValues(prop, 'Function Type');
- 
+
 if (valuetype = 'FormID,Float') and (valuefunctiontype = 'MUL+ADD') then begin
     f := GetNativeValue(ElementByIndex(prop, 7));
 		if f > 1.0 then
@@ -37,12 +44,12 @@ if (valuetype = 'FormID,Float') and (valuefunctiontype = 'MUL+ADD') then begin
 		else
 		  Result := IntToStr(Int(f * 100)) + '%';
 	end
-	
-else if (valuetype = 'FormID,Float') and (valuePropertytype = 'DamageTypeValue') then begin 
+
+else if (valuetype = 'FormID,Float') and (valuePropertytype = 'DamageTypeValue') then begin
     f := GetNativeValue(ElementByIndex(prop, 7));
 		  Result := FloatToStr(f);
 	end
-	
+
 else if valuetype = 'FormID,Float' then begin
 	f := GetNativeValue(ElementByIndex(prop, 7));
 		if f > 5.0 then
@@ -50,7 +57,7 @@ else if valuetype = 'FormID,Float' then begin
 		else if f > 0.0 then
 		  Result := FloatToStr(f)
 	end
-	
+
 else if (valuePropertytype = 'AimModelRecoilArcRotateDeg') or (valuePropertytype = 'AimModelRecoilMinDegPerShot') or (valuePropertytype = 'AimModelRecoilMaxDegPerShot') or (valuePropertytype = 'AimModelRecoilArcDeg') then begin
     f := GetNativeValue(ElementByIndex(prop, 6));
 		if f > 1.0 then
@@ -70,7 +77,7 @@ else if (valuePropertytype = 'AimModelMinConeDegrees') or (valuePropertytype = '
 		else
 		  Result := IntToStr(Int(f * 100)) + '%';
 	end
-	
+
 else if (valuetype = 'Float') and (valuefunctiontype = 'MUL+ADD') then begin
     f := GetNativeValue(ElementByIndex(prop, 6));
 		if f > 1.0 then
@@ -96,27 +103,25 @@ else if valuetype = 'Float' then begin
 		  Result := IntToStr(Int(f * 100)) + '%';
 	end
 
-else if valuetype = 'Int' then begin	
+else if (valuetype = 'FormID,Int') and (valuePropertytype = 'ZoomData') then begin
+		  Result := slPropertyMap.Values[GetEditValue(ElementByIndex(prop, 6))];
+	end
+
+else if (valuetype = 'FormID,Int') then begin
+        Result := '';
+end
+
+else if (valuetype = 'Int') then begin
 	f := GetNativeValue(ElementByIndex(prop, 6));
 		  Result := FloatToStr(f);
 	end
 
-else if (valuetype = 'FormID,Int') and ((valuePropertytype = 'ZoomData') or (valuePropertytype = 'Enchantments')) then begin	
-		  Result := slPropertyMap.Values[GetEditValue(ElementByIndex(prop, 6))];
-	end
-
-else if valuetype = 'FormID,Int' then begin	
-	f := GetNativeValue(ElementByIndex(prop, 6));
-		if f > 1.0 then
-		  Result := FloatToStr(f)
-	end
-
 end;
- 
- 
+
+
  // Property to Name Mapping Layout
 
- 
+
 function GetMappedDescription(prop: IInterface; propname: String): String;
 var
   mappedName, mappedValue, query, queryfunction: String;
@@ -126,44 +131,47 @@ begin
   mappedValue := GetMappedValue(prop);
   query := slPropertyMap.Values[GetEditValue(ElementByIndex(prop, 6))];
   queryfunction := GetElementEditValues(prop, 'Function Type');
-	
+
 	if mappedValue = '\' then
-		Result := Format('%s%s', [mappedName, mappedValue])+ ''
-		
-	else if mappedName = 'Damage_Type' then  
+		Result := Format('%s%s' + '', [mappedName, mappedValue])
+
+	else if mappedName = 'Damage_Type' then
 		Result := Format('Additional ' + '%s' + ' Damage: ' + '%s', [query, mappedValue])
-		
-	else if mappedName = 'Damage_Resistance' then  
+
+	else if mappedName = 'Damage_Resistance' then
 		Result := Format('%s' + ' Damage Reduced by: ' + '%s', [query, mappedValue])
-		
-	else if	(mappedName = 'Actor_Values_Type') and (query <> 'NFW') then
-		Result := Format('%s' '+' + '%s', [query, mappedValue])
-		
-	else if	(mappedName = 'Keywords_Values_Type') or (mappedName = 'MaterialSwaps_Values_Type')  or (mappedName = 'MaterialSwaps_Values_Type') or (mappedName = 'Enchantments_Value') or (mappedName = 'Ammo_Type') then
+
+ 	else if	(mappedName = 'Actor_Values_Type') and (query <> 'NFW') and (query <> '') then
+ 		Result := Format('%s' + '+' + '%s', [query, mappedValue])
+
+	else if	((mappedName = 'Keywords_Values_Type') and (query <> '1')) or ((mappedName = 'MaterialSwaps_Values_Type') and (query <> 'NFW')) or (mappedName = 'Ammo_Type') then
 		Result := Format('%s', [query])
-		
-	else if (mappedName = 'Range (Min\Max):') or (mappedName = 'Recoil (Min\Max):') or (mappedName = 'Cone (Min\Max):') then  
+
+  else if mappedName = 'Enchantments_Value' then
+    Result := Format('%s' + ' ', [query])
+
+	else if (mappedName = 'Range (Min\Max):') or (mappedName = 'Recoil (Min\Max):') or (mappedName = 'Cone (Min\Max):') then
 		Result := Format('%s%s', [mappedName, mappedValue])
-	
-	else  if (query <> 'NFW') then
-		Result := Format('%s%s', [mappedName, mappedValue]); //output layout 
-		
+
+	else if (query <> 'NFW') and (mappedName <> 'Keywords_Values_Type') and (mappedName <> 'Actor_Values_Type') and (mappedName <> 'NFW') and (mappedValue <> 'NFW') then
+		Result := Format('%s%s', [mappedName, mappedValue]); //output layout
+
 end;
- 
+
 function GetOmodDescription(rec: IInterface): String;
 var
   i, j: Integer;
-  prop, properties: IInterface; 
+  prop, properties: IInterface;
   proprefix, prosuffix, propname: string;
   sl: TStringList;
-  
+
 begin
   sl := TStringList.Create;
-	sl.Sorted := true; //sorting automatically happens at insert 
-  
+	sl.Sorted := true; //sorting automatically happens at insert
+
   try
 		properties := ElementByPath(rec, 'DATA\Properties');
-		
+
 		for i := 0 to Pred(ElementCount(properties)) do begin
 			prop := ElementByIndex(properties, i);
 			propname := GetElementEditValues(prop, 'Property');
@@ -183,21 +191,21 @@ begin
 			end
 			else if  Result <> '' then begin
 				 Result := Result + ' | '
-			end
-			Result := Result + Copy(sl[i], 4, Length(sl[i]));
+			end;
+			Result := Result + Copy(sl[i], 4, Length(sl[i]))
 		end
 	finally
 		sl.Free;
 		sl:=nil;
 	end;
 end;
- 
+
 function Initialize: Integer;
 begin
   slPropertyMap := TStringList.Create;
   slPropertyMap.LoadFromFile(sPropertiesList);
 end;
- 
+
 function Process(e: IInterface): Integer;
 var
   desc, oldDesc : string;
@@ -205,26 +213,26 @@ var
 begin
   if Signature(e) <> 'OMOD' then
     Exit;
- 
+
   // patch the winning override record
- 
+
   e := WinningOverride(e);
-	
+
 	if not Assigned(e) then begin
 		AddMessage('something went wrong when getting the override for this record.');
 		Exit;
 	end;
   desc := GetOmodDescription(e);
- 
+
   if desc = '' then
     Exit;
-  
+
   oldDesc := GetEditValue(ElementByPath(e,'DESC'));
   if SameText(oldDesc,desc) then begin
 		AddMessage(Format('description already up to date, ending script - description: "%s"',[desc]));
     Exit;
 	end;
- 
+
   // create new plugin
   if not Assigned(plugin) then begin
     if MessageDlg('Create new patch plugin [YES] or append to the last loaded one [NO]?', mtConfirmation, [mbYes, mbNo], 0) = mrYes then
@@ -236,14 +244,14 @@ begin
       Exit;
     end;
   end;
- 
+
   // skip already copied
   if GetFileName(e) = GetFileName(plugin) then
     Exit;
- 
+
   // add masters
   AddRequiredElementMasters(e, plugin, False);
- 
+
   try
     // copy as override
     r := wbCopyElementToFile(e, plugin, False, True);
@@ -259,9 +267,9 @@ begin
       AddMessage('    reason: ' + Ex.Message);
     end
   end;
- 
+
 end;
- 
+
 function Finalize: Integer;
 begin
   slPropertyMap.Free;
@@ -269,5 +277,5 @@ begin
   if Assigned(plugin) then
     SortMasters(plugin);
 end;
- 
+
 end.
