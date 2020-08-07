de265e6e259c0c1b4d8d98e9c7ec8a77b620922e
Gernash <gernash01@gmail.com>
2020-08-08 07:12:03 +1000

    Update Gernashs description Renamer.pas

--- Gernashs description Renamer.pas
+++ Gernashs description Renamer.pas
@@ -4,9 +4,9 @@
 // Author: Gernash
 //
 
-unit FO4OmodDescriptionsPatcher;
+unit MODRenamer; // FO4PatchOmodDescriptions;
 
-  // code for editing xEdit scripts with Delphi
+// code for editing xEdit scripts with Delphi
 interface
 
 implementation
@@ -27,108 +27,124 @@ var
   g: String;
 
 
-// OMOD Property Value Sort to %, x, deg or Value {{{THE MATHS}}}
-
+  // OMOD Property Value Sort to %, x, deg or Value {{{THE MATHS}}}
 
 begin
-	valuetype := GetElementEditValues(prop, 'Value Type');
+  valuetype := GetElementEditValues(prop, 'Value Type');
   valuePropertytype := GetElementEditValues(prop, 'Property');
   valuefunctiontype := GetElementEditValues(prop, 'Function Type');
 
-if (valuetype = 'FormID,Float') and (valuefunctiontype = 'MUL+ADD') then begin
+  if (valuetype = 'FormID,Float') and (valuefunctiontype = 'MUL+ADD') then
+  begin
     f := GetNativeValue(ElementByIndex(prop, 7));
-		if f > 1.0 then
-		  Result := FloatToStr(f) + 'x'
-   	else if f > 0.0 then
-		  Result := '+' + IntToStr(Int(f * 100)) + '%'
-		else
-		  Result := IntToStr(Int(f * 100)) + '%';
-	end
+    if f > 1.0 then
+      Result := FloatToStr(f) + 'x'
+    else if f > 0.0 then
+      Result := '+' + IntToStr(Int(f * 100)) + '%'
+    else
+      Result := IntToStr(Int(f * 100)) + '%';
+  end
 
-else if (valuetype = 'FormID,Float') and (valuePropertytype = 'DamageTypeValue') then begin
+  else if (valuetype = 'FormID,Float') and
+    (valuePropertytype = 'DamageTypeValue') then
+  begin
     f := GetNativeValue(ElementByIndex(prop, 7));
-		  Result := FloatToStr(f);
-	end
+    Result := FloatToStr(f);
+  end
 
-else if (valuetype = 'FormID,Float') and (valuePropertytype = 'DamageTypeValue') then begin
-	f := GetNativeValue(ElementByIndex(prop, 6));
-		if f > 5.0 then
-		  Result := FloatToStr(f) + '%'
-		else if f > 0.0 then
-		  Result := FloatToStr(f)
-	end
-
-else if valuetype = 'FormID,Float' then begin
-	f := GetNativeValue(ElementByIndex(prop, 7));
-		if f > 5.0 then
-		  Result := FloatToStr(f) + '%'
-		else if f > 0.0 then
-		  Result := FloatToStr(f)
-	end
-
-else if (valuePropertytype = 'AimModelRecoilArcRotateDeg') or (valuePropertytype = 'AimModelRecoilMinDegPerShot') or (valuePropertytype = 'AimModelRecoilMaxDegPerShot') or (valuePropertytype = 'AimModelRecoilArcDeg') then begin
+  else if (valuetype = 'FormID,Float') and
+    (valuePropertytype = 'DamageTypeValue') then
+  begin
     f := GetNativeValue(ElementByIndex(prop, 6));
-		if f > 1.0 then
-		  Result := '+' + FloatToStr(f) + chr($00B0)
-		else if f > 0.0 then
-		  Result := '+' + IntToStr(Int(f * 100)) + chr($00B0)
-		else
-		  Result := IntToStr(Int(f * 100)) + chr($00B0);
-	end
+    if f > 5.0 then
+      Result := FloatToStr(f) + '%'
+    else if f > 0.0 then
+      Result := FloatToStr(f)
+  end
 
-else if (valuePropertytype = 'AimModelMinConeDegrees') or (valuePropertytype = 'AimModelMaxConeDegrees') then begin
+  else if valuetype = 'FormID,Float' then
+  begin
+    f := GetNativeValue(ElementByIndex(prop, 7));
+    if f > 5.0 then
+      Result := FloatToStr(f) + '%'
+    else if f > 0.0 then
+      Result := FloatToStr(f)
+  end
+
+  else if (valuePropertytype = 'AimModelRecoilArcRotateDeg') or
+    (valuePropertytype = 'AimModelRecoilMinDegPerShot') or
+    (valuePropertytype = 'AimModelRecoilMaxDegPerShot') or
+    (valuePropertytype = 'AimModelRecoilArcDeg') then
+  begin
     f := GetNativeValue(ElementByIndex(prop, 6));
-		if f > 1.0 then
-		  Result := FloatToStr(f * 100) + '%'
-		else if f > 0.0 then
-		  Result := '+' + IntToStr(Int(f * 100)) + '%'
-		else
-		  Result := IntToStr(Int(f * 100)) + '%';
-	end
+    if f > 1.0 then
+      Result := '+' + FloatToStr(f) + chr($00B0)
+    else if f > 0.0 then
+      Result := '+' + IntToStr(Int(f * 100)) + chr($00B0)
+    else
+      Result := IntToStr(Int(f * 100)) + chr($00B0);
+  end
 
-else if (valuetype = 'Float') and (valuefunctiontype = 'MUL+ADD') then begin
+  else if (valuePropertytype = 'AimModelMinConeDegrees') or (valuePropertytype = 'AimModelMaxConeDegrees') then
+  begin
     f := GetNativeValue(ElementByIndex(prop, 6));
-		if f > 1.0 then
-		  Result := FloatToStr(f) + 'x'
-		else if f > 0.0 then
-		  Result := '+' + IntToStr(Int(f * 100)) + '%'
-		else
-		  Result := IntToStr(Int(f * 100)) + '%';
-	end
+    if f > 1.0 then
+      Result := FloatToStr(f * 100) + '%'
+    else if f > 0.0 then
+      Result := '+' + IntToStr(Int(f * 100)) + '%'
+    else
+      Result := IntToStr(Int(f * 100)) + '%';
+  end
 
-else if (valuetype = 'Float') and (valuefunctiontype = 'MinRange') then begin
+  else if (valuetype = 'Float') and (valuefunctiontype = 'MUL+ADD') then
+  begin
     f := GetNativeValue(ElementByIndex(prop, 6));
-		  Result := FloatToStr(f) + 'units';
-		end
+    if f > 1.0 then
+      Result := FloatToStr(f) + 'x'
+    else if f > 0.0 then
+      Result := '+' + IntToStr(Int(f * 100)) + '%'
+    else
+      Result := IntToStr(Int(f * 100)) + '%';
+  end
 
-else if valuetype = 'Float' then begin
+  else if (valuetype = 'Float') and (valuefunctiontype = 'MinRange') then
+  begin
     f := GetNativeValue(ElementByIndex(prop, 6));
-		if f > 1.0 then
-		  Result := FloatToStr(f) + 'x'
-		else if f > 0.0 then
-		  Result := '+' + IntToStr(Int(f * 100)) + '%'
-		else
-		  Result := IntToStr(Int(f * 100)) + '%';
-	end
+    Result := FloatToStr(f) + 'units';
+  end
 
-else if (valuetype = 'FormID,Int') and (valuePropertytype = 'ZoomData') then begin
-		  Result := slPropertyMap.Values[GetEditValue(ElementByIndex(prop, 6))];
-	end
+  else if valuetype = 'Float' then
+  begin
+    f := GetNativeValue(ElementByIndex(prop, 6));
+    if f > 1.0 then
+      Result := FloatToStr(f) + 'x'
+    else if f > 0.0 then
+      Result := '+' + IntToStr(Int(f * 100)) + '%'
+    else
+      Result := IntToStr(Int(f * 100)) + '%';
+  end
 
-else if (valuetype = 'FormID,Int') then begin
-        Result := '';
-end
+  else if (valuetype = 'FormID,Int') and (valuePropertytype = 'ZoomData') then
+  begin
+    Result := slPropertyMap.Values[GetEditValue(ElementByIndex(prop, 6))];
+  end
 
-else if (valuetype = 'Int') then begin
-	f := GetNativeValue(ElementByIndex(prop, 6));
-		  Result := FloatToStr(f);
-	end
+  else if (valuetype = 'FormID,Int') then
+  begin
+    Result := '';
+  end
+
+  else if (valuetype = 'Int') then
+  begin
+    f := GetNativeValue(ElementByIndex(prop, 6));
+    Result := FloatToStr(f);
+	//AddMessage(Format('INT: %s', [Result]));
+  end
 
 end;
 
 
- // Property to Name Mapping Layout
-
+// Property to Name Mapping Layout
 
 function GetMappedDescription(prop: IInterface; propname: String): String;
 var
@@ -141,32 +157,44 @@ begin
   query2 := GetElementEditValues(prop, 'Function Type');
   queryfunction := GetElementEditValues(prop, 'Function Type');
 
-	if mappedValue = '\' then
-		Result := Format('%s%s' + '', [mappedName, mappedValue])
+  if mappedValue = '\' then
+    Result := Format('%s%s' + '', [mappedName, mappedValue])
 
-	else if mappedName = 'Damage_Type' then
-		Result := Format('Additional ' + '%s' + ' Damage: ' + '%s', [query, mappedValue])
+  else if mappedName = 'Damage_Type' then
+    Result := Format('Additional ' + '%s' + ' Damage: ' + '%s',
+      [query, mappedValue])
 
-	else if mappedName = 'Damage_Resistance' then
-		Result := Format('%s' + ' Damage Reduced by: ' + '%s', [query, mappedValue])
+  else if mappedName = 'Damage_Resistance' then
+    Result := Format('Reduces ' + '%s' + ' damage by: ' + '%s', [query, mappedValue])
 
- 	else if	(mappedName = 'Actor_Values_Type') and (query <> 'NFW') and (query <> '') then
- 		Result := Format('%s' + '+' + '%s', [query, mappedValue])
- // Ledgendary Effect multiplier
-  	else if	(mappedName = 'Actor_Values_Type') and (query <> 'NFW') and (query = '') then
- 		Result := Format('%s', [mappedValue])
+  else if (mappedName = 'Actor_Values_Type') and (query <> 'NFW') and
+    (query <> '') then
+    Result := Format('%s' + '+' + '%s', [query, mappedValue])
+    // Ledgendary Effect multiplier
+  else if (mappedName = 'Actor_Values_Type') and (query <> 'NFW') and
+    (query = '') then
+    Result := mappedValue
 
-	else if	((mappedName = 'Keywords_Values_Type') and (query <> '1')) or ((mappedName = 'MaterialSwaps_Values_Type') and (query2 <> 'REM') and (query <> 'NFW')) or (mappedName = 'Ammo_Type') then
-		Result := Format('%s', [query])
+  else if ((mappedName = 'Keywords_Values_Type') and (query <> '1')) or ((mappedName = 'MaterialSwaps_Values_Type') and (query2 <> 'REM') and
+    (query <> 'NFW')) or (mappedName = 'Ammo_Type') then
+    Result := query
 
-  	else if (mappedName = 'Enchantments_Value') and (query <> 'NFW') then
-    		Result := Format('%s' + ' ', [query])
+  else if ((mappedName = 'Enchantments_Value') and (query <> 'NFW')) then
+    Result := query
 
-	else if (mappedName = 'Range (Min\Max):') or (mappedName = 'Recoil (Min\Max):') or (mappedName = 'Cone (Min\Max):') then
-		Result := Format('%s%s', [mappedName, mappedValue])
+  else if (mappedName = 'Range (Min\Max):') or
+    (mappedName = 'Recoil (Min\Max):') or (mappedName = 'Cone (Min\Max):') then
+    Result := Format('%s%s', [mappedName, mappedValue]) //or (mappedName = 'Health') 
 
-	else if (query <> 'NFW') and (query2 <> 'REM') and (mappedName <> 'Keywords_Values_Type') and (mappedName <> 'NFW') and (mappedValue <> 'NFW') then //(mappedName <> 'Actor_Values_Type') and
-		Result := Format('%s%s', [mappedName, mappedValue]); //output layout
+  else if (query <> 'NFW') and (query2 <> 'REM') and
+    (mappedName <> 'Keywords_Values_Type') and (mappedName <> 'NFW') and
+    (mappedValue <> 'NFW') then // (mappedName <> 'Actor_Values_Type') and
+    Result := Format('%s%s', [mappedName, mappedValue]); // output layout
+
+   AddMessage(Format('mappedName: %s', [mappedName]));
+   AddMessage(Format('mappedValue: %s', [mappedValue]));
+   AddMessage(Format('query: %s', [query]));
+   AddMessage(Format('query2: %s', [query2]));
 
 end;
 
@@ -179,37 +207,42 @@ var
 
 begin
   sl := TStringList.Create;
-	sl.Sorted := true; //sorting automatically happens at insert
+  sl.Sorted := true; // sorting automatically happens at insert
 
   try
-		properties := ElementByPath(rec, 'DATA\Properties');
+    properties := ElementByPath(rec, 'DATA\Properties');
 
-		for i := 0 to Pred(ElementCount(properties)) do begin
-			prop := ElementByIndex(properties, i);
-			propname := GetElementEditValues(prop, 'Property');
-			j := slPropertyMap.IndexOfName(propname);
-		if j = -1 then Continue;
-			// add property index as prefix for sorting
-			sl.Add( Format('%.3d', [j]) + GetMappedDescription(prop, propname) );
-		end;
+    for i := 0 to Pred(ElementCount(properties)) do
+    begin
+      prop := ElementByIndex(properties, i);
+      propname := GetElementEditValues(prop, 'Property');
+      j := slPropertyMap.IndexOfName(propname);
+      if j = -1 then
+        Continue;
+      // add property index as prefix for sorting
+      sl.Add(Format('%.3d', [j]) + GetMappedDescription(prop, propname));
+    end;
 
-		//concatenate and remove prefixes
+    // concatenate and remove prefixes
 
-		for i := 0 to sl.Count - 1 do begin
-			proprefix := Copy(sl[i], 4, Length(1));
-			prosuffix := RightStr(Result, 1);
-			if  (proprefix = '') or (proprefix = '\') or (prosuffix = ' ')then begin
-				 Result := Result
-			end
-			else if  Result <> '' then begin
-				 Result := Result + ' | '
-			end;
-			Result := Result + Copy(sl[i], 4, Length(sl[i]))
-		end
-	finally
-		sl.Free;
-		sl:=nil;
-	end;
+    for i := 0 to sl.Count - 1 do
+    begin
+      proprefix := Copy(sl[i], 4, Length(1));
+      prosuffix := RightStr(Result, 1);
+      if (proprefix = '') or (proprefix = '\') or (prosuffix = ' ') then
+      begin
+        Result := Result
+      end
+      else if Result <> '' then
+      begin
+        Result := Result + ' | '
+      end;
+      Result := Result + Copy(sl[i], 4, Length(sl[i]))
+    end
+  finally
+    sl.Free;
+    sl := nil;
+  end;
 end;
 
 function Initialize: Integer;
@@ -220,7 +253,7 @@ end;
 
 function Process(e: IInterface): Integer;
 var
-  desc, oldDesc : string;
+  desc, oldDesc: string;
   r: IInterface;
 begin
   if Signature(e) <> 'OMOD' then
@@ -230,28 +263,37 @@ begin
 
   e := WinningOverride(e);
 
-	if not Assigned(e) then begin
-		AddMessage('something went wrong when getting the override for this record.');
-		Exit;
-	end;
+  if not Assigned(e) then
+  begin
+    AddMessage
+      ('something went wrong when getting the override for this record.');
+    Exit;
+  end;
   desc := GetOmodDescription(e);
 
   if desc = '' then
     Exit;
 
-  oldDesc := GetEditValue(ElementByPath(e,'DESC'));
-  if SameText(oldDesc,desc) then begin
-		AddMessage(Format('description already up to date, ending script - description: "%s"',[desc]));
+  oldDesc := GetEditValue(ElementByPath(e, 'DESC'));
+  if SameText(oldDesc, desc) then
+  begin
+    AddMessage
+      (Format('description already up to date, ending script - description: "%s"',
+      [desc]));
     Exit;
-	end;
+  end;
 
   // create new plugin
-  if not Assigned(plugin) then begin
-    if MessageDlg('Create new patch plugin [YES] or append to the last loaded one [NO]?', mtConfirmation, [mbYes, mbNo], 0) = mrYes then
+  if not Assigned(plugin) then
+  begin
+    if MessageDlg
+      ('Create new patch plugin [YES] or append to the last loaded one [NO]?',
+      mtConfirmation, [mbYes, mbNo], 0) = mrYes then
       plugin := AddNewFile
     else
       plugin := FileByIndex(Pred(FileCount));
-    if not Assigned(plugin) then begin
+    if not Assigned(plugin) then
+    begin
       Result := 1;
       Exit;
     end;
@@ -266,15 +308,17 @@ begin
 
   try
     // copy as override
-    r := wbCopyElementToFile(e, plugin, False, True);
-		if not Assigned(r) then begin
-			AddMessage('something went wrong when creating the override record.');
-			Exit;
-		end;
+    r := wbCopyElementToFile(e, plugin, False, true);
+    if not Assigned(r) then
+    begin
+      AddMessage('something went wrong when creating the override record.');
+      Exit;
+    end;
     // setting new description
     SetElementEditValues(r, 'DESC', desc);
   except
-    on Ex: Exception do begin
+    on Ex: Exception do
+    begin
       AddMessage('Failed to copy: ' + FullPath(e));
       AddMessage('    reason: ' + Ex.Message);
     end
@@ -285,7 +329,7 @@ end;
 function Finalize: Integer;
 begin
   slPropertyMap.Free;
-  slPropertyMap:=nil;
+  slPropertyMap := nil;
   if Assigned(plugin) then
     SortMasters(plugin);
 end;
