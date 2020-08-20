61caec644c616a0660805ce6cbc77a11f09bf891
Gernash <gernash01@gmail.com>
2020-08-20 20:51:51 +1000

    Update Gernashs_OMOD_ReNamer_DescParser.pas

--- Gernashs_Lib/Gernashs_OMOD_ReNamer_DescParser.pas
+++ Gernashs_Lib/Gernashs_OMOD_ReNamer_DescParser.pas
@@ -12,10 +12,10 @@ var
   floatValue : Real;
 	prop, properties: IInterface;
 	i,j,dummyInt : Integer;
-	
+
   // OMOD Property Value Sort to %, x, deg or Value {{{THE MATHS}}}
-  
-  
+
+
 begin
 	LogFunctionStart('GetMappedValues');
 	properties := ElementByPath(rec, 'DATA\Properties');
@@ -26,7 +26,7 @@ begin
 			mappedValuesFormat.Add('');
 			continue;
 		end;
-	
+
 		prop := ElementByIndex(properties, i);
 		valuePropertytype := GetElementEditValues(prop, 'Property');
 		j := slPropertyMap.IndexOfName(valuePropertytype);
@@ -35,17 +35,17 @@ begin
 			mappedValuesFormat.Add('');
 			Continue;
 		end;
-		
+
 		loopResult:= '';
 		loopResultFormatted:= '';
-		
+
 		valuetype := GetElementEditValues(prop, 'Value Type');
 		valuePropertytype := GetElementEditValues(prop, 'Property');
 		valuefunctiontype := GetElementEditValues(prop, 'Function Type');
 
-		if (valuePropertytype = 'AimModelRecoilArcRotateDeg') or 
+		if (valuePropertytype = 'AimModelRecoilArcRotateDeg') or
 			(valuePropertytype = 'AimModelMinConeDegrees') or
-			(valuePropertytype = 'AimModelMaxConeDegrees') or  
+			(valuePropertytype = 'AimModelMaxConeDegrees') or
 			(valuePropertytype = 'AimModelRecoilMinDegPerShot') or
 			(valuePropertytype = 'AimModelRecoilMaxDegPerShot') or
 			(valuePropertytype = 'AimModelRecoilHipMult') or
@@ -66,10 +66,10 @@ begin
 				loopResultFormatted := loopResult + chr($00B0);
 			end;
 		end
-		
+
 		else if (valuePropertytype = 'Speed') or
 			(valuePropertytype = 'MinRange') or
-			(valuePropertytype = 'MaxRange') or  
+			(valuePropertytype = 'MaxRange') or
 			(valuePropertytype = 'OutOfRangeDamageMult') or
 			(valuePropertytype = 'AttackActionPointCost') or
 			(valuePropertytype = 'CriticalDamageMult') or
@@ -99,7 +99,7 @@ begin
 					loopResultFormatted := loopResult + '%';
 				end;
 		end
-		
+
 		else if (valuePropertytype = 'Rating') or
 			(valuePropertytype = 'Health') then
 			begin
@@ -118,15 +118,15 @@ begin
 					loopResultFormatted := loopResult + '%';
 				end;
 		end
-			
-		else if (valuePropertytype = 'NumProjectiles') or 
-			(valuePropertytype = 'AimModelRecoilShotsForRunaway')then 
+
+		else if (valuePropertytype = 'NumProjectiles') or
+			(valuePropertytype = 'AimModelRecoilShotsForRunaway')then
 		begin
 			floatValue := GetNativeValue(ElementByIndex(prop, 6));
 			loopResult := FloatToStr(floatValue);
 			loopResultFormatted := loopResult + ' rnd';
-		end	
-		
+		end
+
 		else if (valuePropertytype = 'SightedTransitionSeconds') or
 			(valuePropertytype = 'FullPowerSeconds') or
 			(valuePropertytype = 'AttackDelaySec') then
@@ -146,10 +146,10 @@ begin
 					loopResultFormatted := loopResult + '%';
 				end;
 		end
-		
+
 		else if (valuePropertytype = 'DamageTypeValue') or
-			(valuePropertytype = 'DamageTypeValues') then 
-			begin 
+			(valuePropertytype = 'DamageTypeValues') then
+			begin
 				floatValue := GetNativeValue(ElementByIndex(prop, 7)); // Value 2
 				if floatValue > 1.0 then
 				begin
@@ -165,27 +165,27 @@ begin
 					loopResultFormatted := loopResult + '%';
 				end;
 		end
-		
+
 		else if (valuePropertytype = 'ActorValues') then
 		begin
 			floatValue := GetElementEditValues(prop, 'Value 2'); // Value 2
 			loopResult := IntToStr(floatValue);
 			loopResultFormatted := loopResult;
 			end
-	
-		else if	(valuePropertytype = 'Enchantments') then 
-		begin	
+
+		else if	(valuePropertytype = 'Enchantments') then
+		begin
 		  loopResult := slPropertyMap.Values[RecordToString(LinksTo(ElementByIndex(prop, 6)))];
 		  loopResultFormatted := loopResult;
 		end;
-	
+
      // DebugLog(Format('loopResultFormatted: %s', [loopResultFormatted]));
      //   DebugLog(Format('loopResult: %s', [loopResult]));
-		
+
 		// add property index as prefix for sorting
-		
+
 		mappedValues.Add(loopResult);
-		mappedValuesFormat.Add(loopResultFormatted);
+		mappedValuesFormat.Add(Format('%*.*f', [0, dec, loopResultFormatted]));
 	end;
 	LogFunctionEnd;
 end;
@@ -201,7 +201,7 @@ var
   valuetype2, valuefunctiontype2, valuePropertytype2, value1Loop2, mappedValue2 : string;
   mappedValueFORMAT, mappedValue2FORMAT : string;
   floatValue: Real;
-	prop, properties, prop2 : IInterface; 
+	prop, properties, prop2 : IInterface;
 	mappedValuesFormat, mappedValues, indicesToSkip : TStringList;
 	i,j,k, dummyInt : Integer;
 	lookupStr : String;
@@ -211,24 +211,24 @@ begin
 	indicesToSkip:=TStringList.Create;
 	mappedValuesFormat:=TStringList.Create;
 	indicesToSkip.Sorted:=true; //so that .Find() works
-	
+
 	try
 		GetMappedValues(rec, mappedValues, indicesToSkip, mappedValuesFormat);
 		properties := ElementByPath(rec, 'DATA\Properties');
-		
+
 		for i := 0 to Pred(ElementCount(properties)) do
 		begin
 			loopResult  := '';
-			if indicesToSkip.Find(i,dummyInt) then 
+			if indicesToSkip.Find(i,dummyInt) then
 				continue;
-		
+
 			prop := ElementByIndex(properties, i);
 			valuePropertytype := GetElementEditValues(prop, 'Property');
 			j := slPropertyMap.IndexOfName(valuePropertytype);
-			
+
 			if j = -1 then
 				Continue;
-			
+
 			mappedName := slPropertyMap.Values[valuePropertytype];
 			mappedValue := mappedValues[i];
 			mappedValueFormat := mappedValuesFormat[i];
@@ -236,24 +236,24 @@ begin
 			valuefunctiontype := GetElementEditValues(prop, 'Function Type');
 			valuetype := GetElementEditValues(prop, 'Value Type');
 
-		if mappedName = 'Damage_Type' then 
+		if mappedName = 'Damage_Type' then
 		begin
-			
+
 			for k := 0 to Pred(ElementCount(properties)) do
 			begin
 				if (k=i) then
-					continue; 
+					continue;
 				prop2 := ElementByIndex(properties, k);
-				
+
 				if slPropertyMap.IndexOfName(GetElementEditValues(prop2, 'Property')) = -1 then begin
 					Continue;
-				end; 
-							
+				end;
+
 				valuetype2 := GetElementEditValues(prop2, 'Value Type');
 				valuePropertytype2 := GetElementEditValues(prop2, 'Property');
 				valuefunctiontype2 := GetElementEditValues(prop2, 'Function Type');
 				value1Loop2 := slPropertyMap.Values[RecordToString(LinksTo(ElementByIndex(prop2, 6)))];
-			
+
 				if value1Loop2 = '' then continue;
 
                 if (valuetype2 = 'FormID,Int') and (valuePropertytype2 = 'Keywords') then
@@ -263,24 +263,24 @@ begin
                     break;
 				end;
 			end;
-			
+
 			if loopResult = '' then
 				loopResult := Format('%s damage: %s', [value1Loop1, mappedValueFORMAT]);
 		end
 
-		else if mappedName = 'Damage_Resistance' then 
+		else if mappedName = 'Damage_Resistance' then
 		begin
-		
+
             for k := 0 to Pred(ElementCount(properties)) do
             begin
                 if (k=i) then
-                    continue; 
+                    continue;
 					prop2 := ElementByIndex(properties, k);
 
-                if slPropertyMap.IndexOfName(GetElementEditValues(prop2, 'Property')) = -1 then 
+                if slPropertyMap.IndexOfName(GetElementEditValues(prop2, 'Property')) = -1 then
 				begin
                     Continue;
-                end; 
+                end;
 
                 valuetype2 := GetElementEditValues(prop2, 'Value Type');
                 valuePropertytype2 := GetElementEditValues(prop2, 'Property');
@@ -299,11 +299,11 @@ begin
 
 			if loopResult = '' then
 				loopResult := Format('Reduces %s damage by: %s', [value1Loop1, mappedValueFORMAT]);
-        end 
+        end
 
 		else if (mappedName = 'Range (Min\Max):') or (mappedName = 'Recoil (Min\Max):') or (mappedName = 'Spread (Min\Max):') then
         begin
-		
+
               for k := 0 to Pred(ElementCount(properties)) do
               begin
                   if (k=i) then
@@ -320,7 +320,7 @@ begin
 				  value1Loop2 := GetNativeValue(ElementByIndex(prop2, 6));
 				  mappedValue2 := FloatToStr(value1Loop2);
 
-				
+
          if (mappedName = 'Range (Min\Max):') then
             begin
                     if value1Loop2 > 1.0 then
@@ -336,7 +336,7 @@ begin
 						mappedValue2 := IntToStr(Int(value1Loop2 * 100));
 						mappedValue2FORMAT := mappedValue2 + '%';
 					end;
-			end 
+			end
 
 			else if (mappedName = 'Recoil (Min\Max):') or (mappedName = 'Spread (Min\Max):') then
             begin
@@ -401,10 +401,10 @@ begin
 					end;
         end;
 
-        
+
       end
-				
-			else if (mappedName = 'Actor_Values_Type') and 
+
+			else if (mappedName = 'Actor_Values_Type') and
 				(value1Loop1 <> 'NFW') then
 				begin if (mappedValue = '1') or (value1Loop1 = '') then
 					begin
@@ -412,37 +412,37 @@ begin
 					end	else
 					loopResult := Format('%s' + '+' + '%s', [value1Loop1, mappedValue])
 				end
-				
-				
-		
-				
+
+
+
+
 			else if ((mappedName = 'MaterialSwaps_Values_Type') and
-					(valuetype <> 'REM') and 
-					(value1Loop1 <> 'NFW')) or 				
+					(valuetype <> 'REM') and
+					(value1Loop1 <> 'NFW')) or
 					(mappedName = 'Ammo_Type') then
 				begin
 					loopResult := value1Loop1;
 				end
-			
-			else if ((mappedName = 'Enchantments_Value') and 
-					(value1Loop1 <> 'NFW')) or 
-					((mappedName = 'Keywords_Values_Type') and 
-					(value1Loop1 <> '') and 
-					(value1Loop1 <> 'Energy') and 
-					(value1Loop1 <> 'Cold') and 
-					(value1Loop1 <> 'Radiation') and 
+
+			else if ((mappedName = 'Enchantments_Value') and
+					(value1Loop1 <> 'NFW')) or
+					((mappedName = 'Keywords_Values_Type') and
+					(value1Loop1 <> '') and
+					(value1Loop1 <> 'Energy') and
+					(value1Loop1 <> 'Cold') and
+					(value1Loop1 <> 'Radiation') and
 					(value1Loop1 <> 'Split Beam Shotgun Energy')) then
 				begin
 					loopResult := value1Loop1;
 				end
-				
-			else if (value1Loop1 <> 'NFW') and 
+
+			else if (value1Loop1 <> 'NFW') and
 				(mappedName <> '\') and
-				(valuetype <> 'REM') and 
+				(valuetype <> 'REM') and
 				(mappedName <> 'Keywords_Values_Type') and
-				(mappedName <> 'Actor_Values_Type') and 
+				(mappedName <> 'Actor_Values_Type') and
 				(mappedName <> 'Enchantments_Value') and
-				(mappedName <> 'NFW') and 
+				(mappedName <> 'NFW') and
 				(mappedValue <> 'NFW') then
 				begin
 				loopResult := Format('%s%s', [mappedName, mappedValueFormat]);
@@ -463,7 +463,7 @@ begin
 
 			sl.Add(Format('%.3d', [j]) + loopResult);
 		end;
-		
+
 	finally
 		mappedValues.Free;
 		mappedValues:=nil;
@@ -487,7 +487,7 @@ begin
   sl.Sorted := true; // sorting automatically happens at insert
 
   try
-    
+
 		GetMappedDescription(rec, sl);
 
     // concatenate and remove prefixes
@@ -496,8 +496,8 @@ begin
     begin
       proprefix := Copy(sl[i], 4, Length(1));
       prosuffix := RightStr(Result, 1);
-      if (proprefix = '') or 
-	  (proprefix = '\') or 
+      if (proprefix = '') or
+	  (proprefix = '\') or
 	  (prosuffix = ' ') then
       begin
         Result := Result;
